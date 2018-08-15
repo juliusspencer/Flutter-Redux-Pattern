9fe4faa820064dd68aba12abe8a82d5bc8f106d8
Julius Spencer <jspencer@juliusspencer.co.nz>
2018-08-15 19:40:56 +1200

    Coupla tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,24 +8,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_redux_pattern/main.dart';
+import 'package:flutter_redux_pattern/root/reducer.dart';
+import 'package:redux/redux.dart';
+import 'package:flutter_redux_pattern/models/appState.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-//    final store =
-//    new Store<List<ExampleItem>>(appReducers, initialState: new List());
-//    await tester.pumpWidget(new FlutterReduxApp(store));
-//
-//    // Verify that our counter starts at 0.
-//    expect(find.text('0'), findsOneWidget);
-//    expect(find.text('1'), findsNothing);
-//
-//    // Tap the '+' icon and trigger a frame.
-//    await tester.tap(find.byIcon(Icons.add));
-//    await tester.pump();
-//
-//    // Verify that our counter has incremented.
-//    expect(find.text('0'), findsNothing);
-//    expect(find.text('1'), findsOneWidget);
+    final store = new Store<AppState>(appReducer, initialState: AppState());
+
+    await tester.pumpWidget(new FlutterReduxApp(store));
+
+    // Verify that our counter starts at 0.
+    expect(find.text('Items: 0'), findsOneWidget);
+    expect(find.text('Items: 1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('Items: 0'), findsNothing);
+    expect(find.text('Items: 1'), findsOneWidget);
+
+    // Tap the '-' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.remove));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('Items: 0'), findsOneWidget);
+    expect(find.text('Items: 1'), findsNothing);
   });
 }
